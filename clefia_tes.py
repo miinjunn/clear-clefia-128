d6a593963c6f9b2a563d02e4244679386599dbe0
Munjin Nashida <miinjunn@gmail.com>
2023-11-10 23:35:06 +0700

    added tittle for data processing

--- clefia_tes.py
+++ clefia_tes.py
@@ -1,6 +1,9 @@
 from fungsi import xor_, f0, f1, gFn4_18
 from clefia_key_scheduling import rk
 
+# ------------------------------------------------------------------------------------------
+# DATA PROCESSING
+
 plaintext = [0x00, 0x01, 0x02, 0x03,
              0x04, 0x05, 0x06, 0x07,
              0x08, 0x09, 0x0a, 0x0b,
