c2a9ade5c6b3a40fd95c3c760470422c630206f6
Munjin Nashida <miinjunn@gmail.com>
2023-11-14 01:26:59 +0700

    optimize function

--- file_to_hex.py
+++ file_to_hex.py
@@ -1,49 +1,18 @@
-
 import binascii
 
 
 # file to hex
-with open('baju.webp', 'rb') as fp:
+with open('music.mp3', 'rb') as fp:
     hexstring = binascii.hexlify(fp.read())
 
-# print(hexstring)
-print(type(hexstring))
-print(len(hexstring))
+print(f"panjang file: {len(hexstring)}")
 
-# print(hexstring)
+# decode bytes into str(hex)
 to_hex = hexstring.decode("ascii")
-# print(to_hex)
-print(type(to_hex))
-print(len(to_hex))
-
-# panjang = len(hexstring)
-# print(panjang)
-
-# masukan = panjang // 16
-# print(f"masukan: {masukan}")
-
-# hasil = masukan * 16
-# print(f"hasil: {hasil}")
+print(f"panjang decode: {len(to_hex)}")
 
 
-# with open('new.webp', 'wb') as fp:
-#     fp.write(binascii.unhexlify(to_hex))
-
-# -------------------------------------------------------------------------------------
-
-# halo = 'halosemua'
-# print(halo)
-# halo = [hex(ord(i))[2:] for i in halo]
-# print(halo)
-
-# haloh = ''
-# for i in halo:
-#     haloh += i
-
-# print(haloh)
-# print(len(haloh))
-
-
+# function: split each hex
 def split(state):
     split_2 = []
     for i in range(0, len(state), 2):
@@ -51,51 +20,63 @@ def split(state):
     return split_2
 
 
-# s = split(haloh)
-# print(s)
-# print(len(s))
+# split each hex
+to_each_hex = split(to_hex)
 
-ss = split(to_hex)
-# print(ss)
-print(len(ss))
-ss = [int(i, base=16) for i in ss]
-# print(ss)
-print(len(ss))
+# convert each hex into dec
+to_each_dec = [int(i, base=16) for i in to_each_hex]
 
+# group dec into 1 block (16 bytes)
 split_16 = []
-for i in range(0, len(ss), 16):
-    split_16.append(ss[i: i+16])
+for i in range(0, len(to_each_dec), 16):
+    split_16.append(to_each_dec[i: i+16])
 
-print(split_16[:3])
+
+# --------------------------------------------------------------------------------------------------------
 akan_diconvert = split_16
+
+
+# def convert_per_i(state):
+#     temp = [hex(i)[2:] for i in state]
+#     temp2 = []
+#     for i in temp:
+#         if len(i) == 1:
+#             i = '0' + i
+#         temp2.append(i)
+#     return temp2
 
 
 # nantinya diganti dengan "enkripsi per block"
 # ---------------------------------------------------------------------------------------------
-def convert_per_i(state):
-    temp = [hex(i)[2:] for i in state]
-    temp2 = []
-    for i in temp:
-        if len(i) == 1:
-            i = '0' + i
-        temp2.append(i)
-    return temp2
+# function: convert tiap block menjadi hex
+def convert_per_x(state):
+    temp = []
+    for i in state:
+        cek = hex(i)[2:]
+        if len(cek) == 1:
+            cek = '0' + cek
+        temp.append(cek)
+    return temp
 
 
 # nantinya diganti dengan "enkripsi semua, join hasil enkripsi kedalam 1 variable"
 # ---------------------------------------------------------------------------------------------
+# function: convert tiap block menjadi hex, lalu digabung dalam 1 variable
 def convert_semua(statex):
     semua = ''
     for i in range(len(statex)):
-        temp = convert_per_i(statex[i])
+        temp = convert_per_x(statex[i])
         for item in temp:
             semua += item
     return semua
 
 
-# sem = convert_semua(akan_diconvert)
+sem = convert_semua(akan_diconvert)
 # print(sem)
-# print(len(sem))
+print(len(sem))
+print(type(sem))
 
-# with open('baru.webp', 'wb') as fp:
-#     fp.write(binascii.unhexlify(to_hex))
+
+# hex to file
+# with open('hasil/hasil.mp3', 'wb') as fp:
+#     fp.write(binascii.unhexlify(sem))
