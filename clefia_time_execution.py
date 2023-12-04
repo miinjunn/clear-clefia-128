f12df27088079ac72ef547f3ca48c6650b3503bf
Munjin Nashida <miinjunn@gmail.com>
2023-12-04 08:59:14 +0700

    update function

--- clefia_time_execution.py
+++ clefia_time_execution.py
@@ -1,24 +1,39 @@
 import time
 import os
 
-lokasi = 'python D:/Kuliah/"SMT XI (terakhir)"/"Skripsi XI"/"Clefia-128 python"/clefia_encrypt_file.py'
+lokasi_enc = 'D:/Kuliah/"SMT XI (terakhir)"/"Skripsi XI"/"Clefia-128 python"/clefia_encrypt_file.py'
+lokasi_dec = 'D:/Kuliah/"SMT XI (terakhir)"/"Skripsi XI"/"Clefia-128 python"/clefia_decrypt_file.py'
+
+
+# def repeat(path, iter):
+#     waktuAll = []
+#     i = 0
+#     while i < iter:
+#         print(i)
+#         start = time.time()
+#         os.system('python ' + path)
+#         end = time.time()
+#         waktu = end - start
+#         waktuAll.append(waktu)
+#         i += 1
+#     return waktuAll
 
 
 def repeat(path, iter):
-    temp = []
-    i = 0
-    while i < iter:
+    waktuAll = []
+    for i in range(iter):
         print(i)
-        start = time.time()
-        os.system(path)
-        end = time.time()
+
+        start = time.perf_counter()
+        os.system('python ' + path)
+        end = time.perf_counter()
+
         waktu = end - start
-        temp.append(waktu)
-        i += 1
-    return temp
+        waktuAll.append(waktu)
+    return waktuAll
 
 
-coba = repeat(path=lokasi, iter=100)
+coba = repeat(path=lokasi_dec, iter=100)
 
 print(f"waktu total \t: {coba}")
 print(f"rata-rata \t: {sum(coba)/len(coba)}")
